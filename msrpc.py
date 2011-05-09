db0e70203a3f350db066d6673e8ea156f2d9b224
Sam Thursfield <samthursfield@codethink.co.uk>
2011-05-09 10:23:27 +0100

    Integrate midl compiler to waf build script

---
+++ msrpc.py
@@ -0,0 +1,87 @@
+##
+## Waf support for midl
+## FIXME: could submit this upstream?
+##
+
+import os
+from waflib import Task
+from waflib.TaskGen import before_method, feature, taskgen_method
+
+def process_msrpc_interface(self, type):
+	"""
+	Process the IDL files in *msrpc_interface_for_client* or
+	*msrpc_interface_for_server* and generate C stubs that will
+	be linked to the target.
+	"""
+	if not hasattr(self, 'msrpc_interface'):
+		return
+
+	source_list = self.to_nodes(self.msrpc_interface)
+
+	idl_file_node = source_list[0]
+	if type=='server':
+		c_stub_node = idl_file_node.change_ext('_s.c')
+	elif type=='client':
+		c_stub_node = idl_file_node.change_ext('_c.c')
+	output_list = [c_stub_node]
+
+	c_header = idl_file_node.change_ext('.h')
+
+	# Identical header gets created by both client and server tasks;
+	# ignore it the second time or waf raises an error.
+	header_exists = False
+	for group in self.bld.groups:
+		for task_gen in group:
+			for task in task_gen.tasks:
+				if c_header in task.outputs:
+					header_exists = True
+					break
+
+	if not header_exists:
+		output_list.append (c_header) 
+
+	midl_task = self.create_task('midl', source_list, output_list)
+	midl_task.env['MIDL_WRAPPER_OPTIONS'] = ["-o", "%s" % c_stub_node.bld_dir()]
+	midl_task.env['MIDL_INPUTS'] = [k.abspath() for k in source_list]
+
+	self.source = self.to_nodes(getattr(self, 'source', []))
+	self.source.append(c_stub_node)
+	print("Source for this task %s: %s; output list %s\n" % (self, str(self.source), str(midl_task.outputs)))
+
+
+@feature('msrpc_server')
+@before_method('process_source')
+def process_msrpc_interface_server(self):
+    process_msrpc_interface(self, 'server')
+
+@feature('msrpc_client')
+@before_method('process_source')
+def process_msrpc_interface_client(self):
+    process_msrpc_interface(self, 'client')
+
+class midl(Task.Task):
+	"""
+	Process .IDL files with MIDL.EXE
+	"""
+	run_str = 'sh ${MIDL_WRAPPER} -m ${MIDL} ${MIDL_WRAPPER_OPTIONS} ${MIDL_INPUTS}'
+	vars = ['MIDL']
+	color = 'PINK'
+
+def configure(conf):
+	conf.find_program('midl-wrapper', var='MIDL_WRAPPER')
+	conf.find_program('midl', var='MIDL')
+
+
+# Auxiliary function
+@taskgen_method
+def add_msrpc_interface(self, filename_list):
+	"""
+	Add MSRPC interface definitions, as *msrpc_interface*
+
+	:param filename_list: files
+	:type filename_list: list of string
+	"""
+	if hasattr(self, 'msrpc_interface'):
+		raise Errors.WafError("Only one MSRPC interface permitted per target, on '%s'." % self.name)
+
+	self.msrpc_interface = filename_list
--- wscript
+++ wscript
@@ -3,23 +3,24 @@ bld = 'build'
 top = '.'
 
 def configure(conf):
-	conf.check_tool ('gcc')
+	conf.check_tool ('gcc msrpc')
 
 	# Hack because we are not using MSVC to compile
 	conf.env.DEFINES = 'TARGET_IS_NT351_OR_WIN95_OR_LATER'
 
-	# custom import lib for rpcrt4 as mingw's is too old
-	#conf.env.LIB_RPC_7 = ['rpcrt4-v7']
-	#conf.env.LIBPATH_RPC_7 = ['..']
 	conf.env.LIB_RPC = ['rpcrt4']
 
 def build(bld):
-	client = bld (features = 'c cprogram',
-	              source = ['client.c', 'hello_c.c'],
+	client = bld (features = 'c cprogram msrpc_client',
+	              source = ['client.c'],
+	              msrpc_interface = ['hello.idl', 'hello.acf'],
 	              target = 'client',
+	              includes = ['build'],
 	              use = 'RPC')
 
-	server = bld (features = 'c cprogram',
-	              source = ['server.c', 'hello_s.c'],
+	server = bld (features = 'c cprogram msrpc_server',
+	              source = ['server.c'],
+	              msrpc_interface = ['hello.idl', 'hello.acf'],
 	              target = 'server',
+	              includes = ['build'],
 	              use = 'RPC')
